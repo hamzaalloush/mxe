3b9d69cbb235b59918a854308a954fb99f5c7a7d
Hamza Alloush <hamzaalloush@live.com>
2015-06-05 18:42:14 +0300

    terragear: create initial terragear.mk

---
+++ src/terragear.mk
@@ -0,0 +1,26 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := terragear
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.0
+$(PKG)_CHECKSUM := f5e16880ea94ab1feed98059fb51bbfbcad738c0
+$(PKG)_SUBDIR   := wlbraggs-terragear.git
+$(PKG)_FILE     := wlbraggs-terragear-1a7ecc259b37fec9c5a013249dfe2f4301575a15.tar.bz2
+$(PKG)_URL      := https://gitlab.com/fgear/wlbraggs-terragear/repository/archive.tar.bz2
+$(PKG)_DEPS     := gcc cgal gdal simgear
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://mirrors.ibiblio.org/osgearth/ftp/Source/' | \
+    $(SED) -n 's,.*terragear-\([0-9]*\.[0-9]*[02468]\.[^<]*\)\.tar.*,\1,p' | \
+    grep -v rc | \
+    $(SORT) -V | \
+    tail -1
+endef
+
+define $(PKG)_BUILD
+    mkdir '$(1).build'
+    cd '$(1).build' && cmake '$(1)' \
+        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)'
+    $(MAKE) -C '$(1).build' -j '$(JOBS)' install VERBOSE=1
+endef
